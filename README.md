34c6be15687913fadd3e9d144ac093f1c1e1f084
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-04-23 17:52:53 +0430

    define features name

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Detecting network attack type
 Homework for master course of data mining, Iran university of science and technology
+
+## run
+1. run setup file:
+`python3 setup.py develop`
---
+++ src/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name='network attack type detection',
+    version='0.0.1',
+    author='fatemeh karimi',
+    author_email='fatemehkarimi.001101@gmail.com',
+    packages=['metadata']
+)
